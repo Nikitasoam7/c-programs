1d56c467749b093d14269a8e8bfa22e62f7f2f86
Nikita Soam <109211794+Nikitasoam7@users.noreply.github.com>
2024-10-07 14:55:05 +0530

    Update strlength.C

--- Assignment-18/strlength.C
+++ Assignment-18/strlength.C
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int strlength(char *);
+int main()
+{
+  char st[]="hello! how are you.";
+  int length=strlength(st);
+  printf("length of the string is %d.",length);
+ return 0;
+}
+int strlength(char *p)
+{
+  int i;
+  for (i=0;*(p+i)!='\0';i++);
+  return i;
+}
