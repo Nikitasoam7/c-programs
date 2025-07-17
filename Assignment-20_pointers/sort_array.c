53e88cd4f2b537664934f34d33bebf6f55bd3814
Nikita Soam <109211794+Nikitasoam7@users.noreply.github.com>
2025-07-17 23:03:32 +0530

    Add new Programs

---
+++ Assignment-20_pointers/sort_array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void sort(int *ptr,int size);
+int main()
+{
+    int ar[10];
+    printf("Enter 10 numbers:");
+    for(int i=0;i<10;i++)
+    scanf("%d",&ar[i]);
+    sort(ar,10);
+    printf("After sorting Array\n");
+    for(int i=0;i<10;i++)
+    printf("%d, ",ar[i]);
+    return 0;
+}
+void sort(int *ptr,int size)
+{
+    if(size>1)
+    {
+        int i=1;
+        while(i<size)
+        {
+            int key=ptr[i];
+            int j=i-1;
+            while(j>=0 && key<ptr[gj])
+            {
+                ptr[j+1]=ptr[j];
+                j--;
+            }
+            ptr[j+1]=key;
+            i++;
+        }
+    }
+}
